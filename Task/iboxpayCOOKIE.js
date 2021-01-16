638cabf0c217d27b30d55c4b2a0a6000e4a415a8
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-01-16 22:09:51 +0800

    Add files via upload

---
+++ Task/iboxpayCOOKIE.js
@@ -0,0 +1,16 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let iboxpayheaderVal= ``
+
+
+
+let iboxpaycookie = {
+  iboxpayheaderVal: iboxpayheaderVal,  
+  
+}
+
+module.exports =  iboxpaycookie
+  
+
+
