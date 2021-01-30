0f101eb5dec3865b2c73f0574d23ac5e0ffbe1df
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-01-30 17:18:05 +0800

    Update iboxpayCOOKIE.js

--- Task/iboxpayCOOKIE.js
+++ Task/iboxpayCOOKIE.js
@@ -2,11 +2,12 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 let iboxpayheaderVal= ``
-
+let refreshtokenVal= ``
 
 
 let iboxpaycookie = {
   iboxpayheaderVal: iboxpayheaderVal,  
+  refreshtokenVal: refreshtokenVal,  
   
 }
 
