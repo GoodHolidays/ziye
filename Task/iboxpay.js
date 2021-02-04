9c5867a5654ce24df0de46d1affb5b06be1a0b98
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-02-04 14:35:48 +0800

    笑谱   修复金蛋报错问题，增加视频收益统计

--- Task/iboxpay.js
+++ Task/iboxpay.js
@@ -27,6 +27,7 @@ boxjs链接  https://raw.githubusercontent.com/ziye12/JavaScript/main/Task/ziye.
 2.3 修复直播问题，采用真实直播id
 2.3 设置LIVE 为61 时  单跑直播
 2.3 修复错误，修复直播收益显示
+2.4 修复金蛋报错问题，增加视频收益统计
 
 ⚠️一共2个位置 2个ck  👉 3条 Secrets 
 多账号换行
@@ -81,6 +82,7 @@ const notifyInterval = 2; // 0为关闭通知，1为所有通知，2为12 23 点
 const CS = 6
 $.message = '', COOKIES_SPLIT = '', CASH = '', LIVE = '', ddtime = '', spid = '', TOKEN = '', zbid = '', cashcs = '', newcashcs = '', liveId = '';
 let livecs = 0,
+    videoscs = 0,
     liveIdcd = 0;
 RT = 30000;
 const iboxpayheaderArr = [];
@@ -296,36 +298,37 @@ async function all() {
         await cktime(); //CK获取时间
         await goldcoin(); //金币信息
         await coin(); //账户信息
+        await sylist(); //收益列表
+        await splimit(); //视频上限
         await hdid(); //活动id
         await newcashlist(); //提现查询
         await cashlist(); //今日提现查询
         if (!cashcs.amount && CASH >= 1 && $.coin.data.balance / 100 >= CASH) {
             await withdraw(); //提现
         }
-        if (LIVE >= 1 && nowTimes.getHours() >= 8 && nowTimes.getHours() <= 23) {
-            await sylist(); //收益列表
-            if ($.sylist.resultCode && livecs < LIVE) {
-                await liveslist(); //直播节目表
-                await lives(); //看直播
-            }
+        if (LIVE >= 1 && nowTimes.getHours() >= 8 && nowTimes.getHours() <= 23 && $.sylist.resultCode && livecs < LIVE) {
+            await liveslist(); //直播节目表
+            dd = liveIdcd * 35 - 34
+            console.log(`📍本次直播运行需要${dd}秒` + '\n')
+            await lives(); //看直播
+            await $.wait(dd * 1000)
+
         }
 
-        if (liveIdcd < CS && LIVE != 61) {
-            dd = CS * 35
-        } else dd = liveIdcd * 35
-
-        console.log(`📍本次运行等待${dd}秒` + '\n')
         if (LIVE != 61) {
+            tt = CS * 30 - 29
+            console.log(`📍本次视频运行需要${tt}秒` + '\n')
             await play(); //播放       
             await video(); //视频
+            await $.wait(tt * 1000)
             if (!newcashcs.amount) {
                 await newvideo(); //新人福利
             }
-            if ($.video.data && $.video.data.goldCoinNumber != 0) {
+            if ($.video.data && $.video.data.goldCoinNumber != 0 && videoPublishId6) {
                 await goldvideo(); //金蛋视频
             }
         }
-        await $.wait(dd * 1000)
+
     }
 }
 //通知
@@ -428,7 +431,7 @@ function goldcoin(timeout = 0) {
                 try {
                     if (logs) $.log(`${O}, 金币信息🚩: ${data}`);
                     $.goldcoin = JSON.parse(data);
-                    $.message += '【金币信息】：今日金币' + $.goldcoin.data.coinSum + ',预估金额' + $.goldcoin.data.balanceSum / 100 + '元' + '\n';
+                    $.message += '【金币信息】：今日金币' + $.goldcoin.data.coinSum + ',预估金额' + $.goldcoin.data.balanceSum / 100 + '元\n';
                 } catch (e) {
                     $.logErr(e, resp);
                 } finally {
@@ -479,7 +482,7 @@ function coin(timeout = 0) {
                 try {
                     if (logs) $.log(`${O}, 账户信息🚩: ${data}`);
                     $.coin = JSON.parse(data);
-                    $.message += '【账户信息】：可提余额' + $.coin.data.balance / 100 + ',明日入账' + $.coin.data.tomorrowAmt / 100 + '元' + '\n';
+                    $.message += '【账户信息】：明日入账' + $.coin.data.tomorrowAmt / 100 + '元,可提余额' + $.coin.data.balance / 100 + '元\n';
                 } catch (e) {
                     $.logErr(e, resp);
                 } finally {
@@ -755,17 +758,54 @@ function sylist(timeout = 0) {
                 try {
                     if (logs) $.log(`${O}, 收益列表🚩: ${data}`);
                     $.sylist = JSON.parse(data);
-                    if ($.sylist.resultCode == 1 && data.match(/:500,/g)) {
-                        live = data.match(/:500,/g);
+
+                    if ($.sylist.resultCode == 1 && data.match(/"actTypeId":10,/g)) {
+                        live = data.match(/"actTypeId":10,/g);
                         livecs = live.length;
-                        console.log('已获得直播奖励 ' + livecs + ' 次，共' + livecs * 500 + '金币\n')
-                        $.message +=
-                            '【直播收益】：已获得直播奖励 ' + livecs + ' 次，共' + livecs * 500 + '金币\n'
                     } else livecs = 0
+
+                    if ($.sylist.resultCode == 1 && data.match(/"actTypeId":9,/g)) {
+
+                        videos = data.match(/"actTypeId":9,/g);
+                        videoscs = videos.length;
+                    } else videoscs = 0
+                    console.log('已获得直播奖励 ' + livecs + ' 次，共' + livecs * 500 + '金币\n')
+                    $.message +=
+                        '【直播收益】：已获得直播奖励 ' + livecs + ' 次，共' + livecs * 500 + '金币\n'
+                    console.log('已获得视频奖励 ' + videoscs + ' 次，共' + ($.goldcoin.data.coinSum - livecs * 500) + '金币\n')
+                    $.message +=
+                        '【视频收益】：已获得视频奖励 ' + videoscs + ' 次，共' + ($.goldcoin.data.coinSum - livecs * 500) + '金币\n'
+
                     if ($.sylist.resultCode == 0) {
                         console.log($.sylist.errorDesc + '\n');
                         $.message +=
-                            '【直播收益】：' + $.sylist.errorDesc + '\n';
+                            '【收益列表】：' + $.sylist.errorDesc + '\n';
+                    }
+                } catch (e) {
+                    $.logErr(e, resp);
+                } finally {
+                    resolve()
+                }
+            })
+        }, timeout)
+    })
+}
+//视频上限  
+function splimit(timeout = 0) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            header = iboxpayheaderVal.replace(`${token}`, `${TOKEN}`).replace(`${oldtime}`, `${tts()}`)
+            let url = {
+                url: `https://veishop.iboxpay.com/nf_gateway/nf_customer_activity/day_cash/ignore_tk/v1/get_video_act.json`,
+                headers: JSON.parse(header),
+            }
+            $.get(url, async (err, resp, data) => {
+                try {
+                    if (logs) $.log(`${O}, 视频上限🚩: ${data}`);
+                    $.splimit = JSON.parse(data);
+                    if ($.splimit.resultCode == 1) {
+                        console.log('视频上限：今日上限' + $.splimit.data.goldCoinDayLimit + '金币,今日未得' + ($.splimit.data.goldCoinDayLimit - ($.goldcoin.data.coinSum - livecs * 500)) + '金币\n');
+                        $.message += '【视频上限】：今日上限' + $.splimit.data.goldCoinDayLimit + '金币,今日未得' + ($.splimit.data.goldCoinDayLimit - ($.goldcoin.data.coinSum - livecs * 500)) + '金币\n';
                     }
                 } catch (e) {
                     $.logErr(e, resp);
