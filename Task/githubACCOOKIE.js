6488dda7334e52a7c72ddbb829bad25af7b66ef5
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-02-07 00:06:37 +0800

    Add files via upload

---
+++ Task/githubACCOOKIE.js
@@ -0,0 +1,25 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let githubACnamebodyVal= ``
+let githubACurlVal= ``
+let githubACheaderVal= ``
+let githubACbodyVal= ``
+
+
+
+
+let githubACcookie = {
+  githubACnamebodyVal: githubACnamebodyVal, 
+  githubACurlVal: githubACurlVal,  
+  githubACheaderVal: githubACheaderVal,  
+  githubACbodyVal: githubACbodyVal, 
+
+
+
+}
+
+module.exports =  githubACcookie
+  
+
+
