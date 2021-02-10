96c92672f9afd81f57837686b9e9d2f337623c90
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-02-10 23:29:52 +0800

    Update duokandianCOOKIE.js

--- Task/duokandianCOOKIE.js
+++ Task/duokandianCOOKIE.js
@@ -1,12 +1,13 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 let duokandianbodyVal = ``
-
+let duokandianvideobodyVal = ``
 
 
 
 let duokandiancookie = {
-    duokandianbodyVal: duokandianbodyVal,
 
+    duokandianbodyVal: duokandianbodyVal,
+    duokandianvideobodyVal: duokandianvideobodyVal,
 }
 
 module.exports = duokandiancookie
