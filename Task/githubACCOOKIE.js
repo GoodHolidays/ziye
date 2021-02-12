f3a06541d59923719b8183b860beca049d06575e
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-02-12 15:48:36 +0800

    Update githubACCOOKIE.js

--- Task/githubACCOOKIE.js
+++ Task/githubACCOOKIE.js
@@ -1,4 +1,4 @@
-//独立COOKIE文件     ck在``里面填写，多js换行
+//独立COOKIE文件     ck在``里面填写，多js换行，不填也要换行
 let githubACnameVal = ``
 let githubACurlVal = ``
 let githubACheaderVal = ``
@@ -6,6 +6,16 @@ let githubACbodyVal = ``
 
 let HHAVal = ``
 let HHBVal = ``
+let HHCVal = ``
+let HHDVal = ``
+let HHEVal = ``
+let HHFVal = ``
+let HHGVal = ``
+let HHHVal = ``
+let HHIVal = ``
+let HHJVal = ``
+let HHKVal = ``
+let HHLVal = ``
 
 let MMAVal = ``
 let MMBVal = ``
@@ -31,13 +41,23 @@ let githubACcookie = {
 
     HHAVal: HHAVal,
     HHBVal: HHBVal,
+    HHCVal: HHCVal,
+    HHDVal: HHDVal,
+    HHEVal: HHEVal,
+    HHFVal: HHFVal,
+    HHGVal: HHGVal,
+    HHHVal: HHHVal,
+    HHIVal: HHIVal,
+    HHJVal: HHJVal,
+    HHKVal: HHKVal,
+    HHLVal: HHLVal,
     MMAVal: MMAVal,
     MMBVal: MMBVal,
     MMCVal: MMCVal,
     MMDVal: MMDVal,
     MMEVal: MMEVal,
     MMFVal: MMFVal,
-	MMGVal: MMGVal,
+    MMGVal: MMGVal,
     MMHVal: MMHVal,
     MMIVal: MMIVal,
     MMJVal: MMJVal,
