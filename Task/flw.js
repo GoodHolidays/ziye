9a336e70927076e6a73b2ddca996647f5fa7cadd
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-02-12 15:55:52 +0800

    Update flw.js

--- Task/flw.js
+++ Task/flw.js
@@ -281,14 +281,16 @@ function daytime(inputTime) {
 };
 //时间戳格式化日期
 function time(inputTime) {
-  var date = new Date(inputTime);
-  Y = date.getFullYear() + '-';
-  M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  D = date.getDate() + ' ';
-  h = date.getHours() + ':';
-  m = date.getMinutes() + ':';
-  s = date.getSeconds();
-  return Y + M + D + h + m + s;
+    if ($.isNode()) {
+        var date = new Date(inputTime + 8 * 60 * 60 * 1000);
+    } else var date = new Date(inputTime);
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    D = date.getDate() + ' ';
+    h = date.getHours() + ':';
+    m = date.getMinutes() + ':';
+    s = date.getSeconds();
+    return Y + M + D + h + m + s;
 };
 let isGetCookie = typeof $request !== 'undefined'
 if (isGetCookie) {
